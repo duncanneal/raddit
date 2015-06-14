9086ba36bd56929c524107b8dae83e9c5ae209de
Duncan Neal <duncan.neal@gmail.com>
2015-06-14 17:12:46 -0400

    most recent

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :comments
+  
 
   devise_for :users
   resources :links do
     member do
-      put "like", to:    "links#upvote"
+      put "like",    to: "links#upvote"
       put "dislike", to: "links#downvote"
     end
     resources :comments
-    end
+  end
   root "links#index"
 
   # Example of regular route:
